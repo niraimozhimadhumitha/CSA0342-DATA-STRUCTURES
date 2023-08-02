2fb6c1063e634c9b7a8a87b4a0ef649b3f69b641
niraimozhimadhumitha <131957887+niraimozhimadhumitha@users.noreply.github.com>
2023-08-02 10:46:51 +0530

    Update implement array operations.cpp

--- implement array operations.cpp
+++ implement array operations.cpp
@@ -1,116 +1,36 @@
 #include <stdio.h>
 #define MAX_SIZE 100
-int main() {
-    int array[MAX_SIZE];
-    int n, pos, value;
-    
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
-    
-    printf("Enter the elements:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &array[i]);
+int insert(int arr[], int size, int index, int value) {
+    if (size >= MAX_SIZE || index < 0 || index > size)
+        return size;
+    for (int i = size - 1; i >= index; i--) {
+        arr[i + 1] = arr[i];
+    }   
+    arr[index] = value;
+    return size + 1;
 }
-    printf("Enter the position where the element should be inserted: ");
-    scanf("%d", &pos);
-    
-    switch (pos) {
-        case 1:
-            for (int i = n - 1; i >= 0; i--) {
-                array[i + 1] = array[i];
-            }
-            printf("Enter the value of the new element: ");
-            scanf("%d", &value);
-            array[0] = value;
-            n++;
-            printf("Element inserted successfully.\n");
-            break;
-        
-        case 2:
-            printf("Enter the value of the new element: ");
-            scanf("%d", &value);
-            array[n] = value;
-            
-            n++;
-            
-            printf("Element inserted successfully.\n");
-            break;
-        
-        default:
-            if (pos >= 3 && pos <= n + 1) {
-                for (int i = n - 1; i >= pos - 1; i--) {
-                    array[i + 1] = array[i];
-                }
-                printf("Enter the value of the new element: ");
-                scanf("%d", &value);
-                array[pos - 1] = value;
-                n++;
-                
-                printf("Element inserted successfully.\n");
-            } else {
-                printf("Invalid position.\n");
-            }
-            break;
+int deleteElement(int arr[], int size, int index) {
+    if (size <= 0 || index < 0 || index >= size)
+        return size; 
+    for (int i = index; i < size - 1; i++) {
+        arr[i] = arr[i + 1];
+    }   
+    return size - 1;
+}
+int display(int arr[], int size) {
+    printf("Array elements: ");
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
     }
-    
-    printf("Updated array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", array[i]);
-    }
-    
+    printf("\n");
+}
+int main() {
+    int arr[MAX_SIZE] = {1, 2, 3, 4, 5};
+    int size = 5;
+    display(arr, size);
+    size = insert(arr, size, 2, 10);
+    display(arr, size);
+    size = deleteElement(arr, size, 3);
+    display(arr, size);
     return 0;
 }
-#include <stdio.h>
-#define MAX_SIZE 100
-
-int main() {
-    int array[MAX_SIZE];
-    int n, pos;
-    
-    printf("Enter the number of elements in the array: ");
-    scanf("%d", &n);
-    
-    printf("Enter the elements:\n");
-    for (int i = 0; i < n; i++) {
-        scanf("%d", &array[i]);
-    }
-    
-    printf("Enter the position of the element to be deleted: ");
-    scanf("%d", &pos);
-    
-    switch (pos) {
-        case 1:
-            for (int i = 0; i < n - 1; i++) {
-                array[i] = array[i + 1];
-            }
-            n--;
-            
-            printf("Element deleted successfully.\n");
-            break;
-        
-        case 2:
-             the size of the array
-            n--;
-            
-            printf("Element deleted successfully.\n");
-            break;
-        
-        default:
-            if (pos >= 3 && pos <= n) {
-                for (int i = pos - 1; i < n - 1; i++) {
-                    array[i] = array[i + 1];
-            }
-                n--;           
-                printf("Element deleted successfully.\n");
-            } else {
-                printf("Invalid position.\n");
-            }
-            break;
-    }
-    printf("Updated array: ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", array[i]);
-    }
-    return 0;
-}
-
